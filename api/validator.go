ec227318f5156476fdb80dc34bf9680579c5f5cd
Vlad <vladvolkov03@mail.ru>
2022-07-30 22:11:23 +0300

    added some server func to transfer money

--- api/validator.go
+++ api/validator.go
@@ -5,6 +5,7 @@ import (
 	"transactions/util"
 )
 
+
 var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
 	if currency, ok := fieldLevel.Field().Interface().(string); ok {
 		return util.IsSupportedCurrency(currency)
